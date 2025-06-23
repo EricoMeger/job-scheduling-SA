d3508e7e7d5c65f25a46fc4200afed89ccb31339
EricoMeger <ericomeger9@gmail.com>
2025-06-23 09:25:24 -0300

    generate neighbor solutions

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 def initialize_assignments(tasks, machines):
     assignments = []
     for _ in range(machines):
@@ -21,9 +23,33 @@ def calculate_makespan(tasks, assignments):
 
     return max(cost)
 
+def find_task(selected_task, state):
+    for i in range(len(state)):
+        if state[i][selected_task] == 1:
+            return i
+        
+    return -1
+    
+def generate_neighbor(current_state):
+    selected_task = random.randint(0, len(current_state) - 1)
+    
+    machine_from = find_task(selected_task, current_state)
+    machine_to = machine_from
+    
+    while machine_to == machine_from:
+        machine_to = random.randint(0, len(current_state) - 1)
+    
+    current_state[machine_from][selected_task] = 0
+    current_state[machine_to][selected_task] = 1
+    
+    return current_state
 
 tasks = [1, 2, 3, 4, 5]
 machines = 2
 machine_assignments = create_initial_solution(tasks, machines)
+
+print(machine_assignments)
+
+generate_neighbor(machine_assignments)
 
 print(machine_assignments)
